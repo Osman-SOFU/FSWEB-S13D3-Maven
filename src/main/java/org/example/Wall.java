1582933a64a0422048c9f5e78718a9a692a12c26
Osman-SOFU <osofu1903@gmail.com>
2025-01-08 14:43:27 +0300

    Missions completed.

---
+++ src/main/java/org/example/Wall.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Wall {
+    double width;
+    double height;
+
+    public Wall(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double setWidth(double sayi){
+        if(sayi < 0){
+           return width = 0;
+        }else {
+           return width = sayi;
+        }
+    }
+
+    public double setHeight(double sayi){
+        if(sayi < 0){
+          return height = 0;
+        }else {
+            return height = sayi;
+        }
+    }
+
+    public double getArea(){
+        return width*height;
+    }
+}
